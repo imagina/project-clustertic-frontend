227ab9beadfbbc84dbb060e9a870e1d214bfeb4a
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-10-23 16:21:17 -0500

    + page logout

---
+++ pages/auth/logout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const store = useAuthStore()
+definePageMeta({
+  middleware: 'auth',
+})
+
+onMounted(() => {
+  store.logout()
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>logout page</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
